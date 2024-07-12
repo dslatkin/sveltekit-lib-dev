1d93ece45a8d088c1baa637768d10d3589b6c96b
Daniel Slatkin <danielslatkin@gmail.com>
2024-07-12 21:34:23 +0000

    add distributed lib to /vite

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,12 @@ export default defineConfig({
       customElement: true
     }
   })],
+  build: {
+    // https://vitejs.dev/guide/build.html#library-mode
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'MyWcLib',
+      fileName: 'my-wc-lib',
+    }
+  }
 })
