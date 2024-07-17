1a0ce66c9f3e53d81cc1b3ef71f24f6b26cb8da6
Daniel Slatkin <danielslatkin@gmail.com>
2024-07-17 19:29:29 +0000

    add nest components example

---
+++ vite/src/lib/Inner.wc.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  console.log('Loading inner');
+
+  onMount(() => {
+    console.log('Inner mounted')
+  })
+</script>
+
+<svelte:options customElement="my-inner" />
+
+<div>Inner</div>
+
+<style>
+  div {
+    background: lightblue;
+    padding: 0.5rem;
+  }
+</style>
---
+++ vite/src/lib/Outer.wc.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  console.log('Loading outer');
+
+  onMount(() => {
+    console.log('Outer mounted')
+  })
+
+</script>
+
+<svelte:options customElement="my-outer" />
+
+<div>
+  Outer start
+  <slot />
+  Outer end
+</div>
+
+<style>
+  div {
+    background: pink;
+    padding: 0.5rem;
+  }
+</style>
